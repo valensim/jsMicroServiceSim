329b0ed59e77e19ed7bc5e7e412018ec90142315
Simon Valenta <simon.valenta@emplifi.io>
2024-12-11 16:14:22 +0100

    adding more jobs and datasets and the ability to run input from file

--- cli.js
+++ cli.js
@@ -1,25 +1,44 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
 import readline from 'readline';
-import {s3, kafka, rabbit, elastic, kinesis, dynamo} from "./datasets.js";
+import {
+  s3,
+  kafka,
+  rabbit,
+  elastic,
+  kinesis,
+  dynamo,
+  api,
+  postgres
+} from "./datasets.js";
 import {Service} from "./service.js";
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 const producer = 'https://github.com/valensim/OpenLineageJSClient';
 const schemaURL = 'https://github.com/valensim/OpenLineageJSClient';
 
 // create  a list of the services match them based on the jobName and then call them in the switch
-let replication = new Service('replication', 'replicationNamespace', producer, schemaURL, [dynamo], [s3, rabbit, elastic]);
-let fetching = new Service('fetching', 'fetchingNamespace', producer, schemaURL, [kafka], [dynamo]);
-let enrichment = new Service('enrichment', 'enrichmentNamespace', producer, schemaURL, [kinesis, dynamo], [dynamo]);
-let jobs = [replication, fetching, enrichment];
+let transform = new Service('transform', 'transformNamespace', producer, schemaURL, [kinesis], [dynamo]);
+let saver = new Service('saver', 'saverNamespace', producer, schemaURL, [kafka, api], [dynamo, rabbit]);
+let analysis = new Service('analysis', 'analysisNamespace', producer, schemaURL, [s3], [postgres, s3]);
+let replication = new Service('replication', 'replicationNamespace', producer, schemaURL, [dynamo], [s3, elastic]);
+let enrichment = new Service('enrichment', 'enrichmentNamespace', producer, schemaURL, [rabbit], [kinesis]);
+let jobs = [replication, saver, enrichment, transform, analysis];
 let names = jobs.map((job) => job.jobName);
 
 console.log('Enter a name of the service (' + names + ') and a command (start, running, end, latest):');
+
+const inputFile = process.argv[2];
+let inputStream = process.stdin;
+
+if (inputFile) {
+  inputStream = fs.createReadStream(inputFile);
+}
+
+const rl = readline.createInterface({
+  input: inputStream,
+  output: process.stdout
+});
 
 rl.on('line', (input) => {
   if (input.trim() === 'exit') {
@@ -32,9 +51,14 @@ rl.on('line', (input) => {
 	return;
   }
   let jobName = input[0].trim();
-
   let command = input[1].trim();
+
   let job = jobs.find((job) => job.jobName === jobName);
+  if (!job) {
+	console.log('Unknown job name. Use one of the following:', names);
+	return;
+  }
+
   switch (command) {
 	case 'latest':
 	  job.beLastRun();
--- datasets.js
+++ datasets.js
@@ -6,5 +6,7 @@ let s3 = new InputDatasetBuilder().setName('S3_path').setNamespace('S3_bucket_an
 let rabbit = new InputDatasetBuilder().setName('rabbit').setNamespace('rabbit_namespace').build();
 let elastic = new InputDatasetBuilder().setName('elastic_index_pattern').setNamespace('elastic_host').build();
 let dynamo = new InputDatasetBuilder().setName('Dynamo_Table').setNamespace('Dynamo_Region').build();
+let postgres = new InputDatasetBuilder().setName('Postgres_Table').setNamespace('Postgres_Host').build();
+let api = new InputDatasetBuilder().setName('API').setNamespace('API_URL').build();
 
-export {kinesis, kafka, s3, rabbit, elastic, dynamo};
+export {kinesis, kafka, s3, rabbit, elastic, dynamo, postgres, api};
